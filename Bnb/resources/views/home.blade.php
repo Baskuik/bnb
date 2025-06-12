4c53f04cf1d7b87a17d60e0f9c5b75331c114ecb
Baskuik <97105236@st.deltion.nl>
2025-06-12 19:02:19 +0200

    home pagina en font awesome

---
+++ Bnb/resources/views/home.blade.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Home</title>
+</head>
+<style>
+    .body {
+        font-family: 'Calibri';
+    }
+</style>
+
+<<body>
+    @include('layout')
+
+    <div class="voorzieningen-container">
+        <ul class="voorzieningen-lijst">
+            <li><i class="fa-solid fa-euro-sign"></i> 120,- Per nacht</li>
+            <li><i class="fa-solid fa-wifi"></i> Gratis wifi</li>
+            <li><i class="fa-solid fa-eye"></i> Uitzicht</li>
+            <li><i class="fa-solid fa-house"></i> Je hebt de accommodatie voor jezelf</li>
+            <li><i class="fa-solid fa-ruler-combined"></i> 50m² grootte</li>
+            <li><i class="fa-solid fa-square-parking"></i> Gratis parkeren</li>
+            <li><i class="fa-solid fa-toilet-paper"></i> Badkamer</li>
+            <li><i class="fa-solid fa-ban-smoking"></i> Rookvrij</li>
+            <li><i class="fa-solid fa-kitchen-set"></i> Keuken</li>
+            <li><i class="fa-solid fa-shower"></i> Douche</li>
+            <li><i class="fa-solid fa-umbrella-beach"></i> Terras</li>
+        </ul>
+    </div>
+</body>
+
+</html>
---
+++ Bnb/resources/views/layout.blade.php
@@ -0,0 +1,52 @@
+<!DOCTYPE html>
+<html lang="nl">
+
+<head>
+   <script src="https://kit.fontawesome.com/fa79014ad0.js" crossorigin="anonymous"></script>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>@yield('title', 'Bnb')</title>
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css">
+</head>
+<style>
+    .navbar {
+        font-family: 'Calibri';
+    }
+</style>
+<body>
+
+    {{-- Navbar --}}
+    <nav class="navbar navbar-expand-lg">
+        <div class="container-fluid">
+            <a class="navbar-brand" href="/">MijnSite</a>
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse" id="navbarNav">
+                <ul class="navbar-nav ms-auto">
+                    <li class="nav-item">
+                        <a style="color: blue; font-size: 20px !important;" class="nav-link" href="/">Home</a>
+                    </li>
+                    <li class="nav-item">
+                        <a style="color: blue; font-size: 20px !important;" class="nav-link" href="/booking">Booking</a>
+                    </li>
+                    <li class="nav-item">
+                        <a style="color: blue; font-size: 20px !important;" class="nav-link" href="/reviews">Reviews</a>
+                    </li>
+                    <li class="nav-item">
+                        <a style="color: blue; font-size: 20px !important;" class="nav-link" href="/details">Details</a>
+                    </li>
+                </ul>
+            </div>
+        </div>
+    </nav>
+
+    {{-- Content van elke pagina --}}
+    <div class="container mt-4">
+        @yield('content')
+    </div>
+
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js"></script>
+</body>
+
+</html>
