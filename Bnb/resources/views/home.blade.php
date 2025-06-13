c7a3720804db3484b580ea6d902cd7e41779c82e
Baskuik <97105236@st.deltion.nl>
2025-06-13 15:46:12 +0200

    foto word vergroot wnr geklikt

--- Bnb/resources/views/home.blade.php
+++ Bnb/resources/views/home.blade.php
@@ -1,171 +1,116 @@
 <!DOCTYPE html>
+<html lang="nl">
 
 <head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <meta charset="UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1" />
     <title>Home</title>
-    <link rel="stylesheet" href="{{ asset('css/style.css') }}">
+    <link rel="stylesheet" href="{{ asset('css/style.css') }}" />
 </head>
-<style>
-    .body {
-        font-family: 'Calibri';
-    }
-</style>
 
 <body>
     @include('layout')
-  <div class="slider-container">
-    <button class="arrow arrow-left">&lt;</button>
 
-    <div class="slider">
-        <img src="{{ asset('images/bnbfoto1.png') }}" class="slider-img active" alt="bnbfoto1">
-        <img src="{{ asset('images/bnbfoto2.png') }}" class="slider-img" alt="bnbfoto2">
-        <img src="{{ asset('images/bnbfoto3.png') }}" class="slider-img" alt="bnbfoto3">
-        <img src="{{ asset('images/bnbfoto4.png') }}" class="slider-img" alt="bnbfoto4">
-        <img src="{{ asset('images/bnbfoto5.png') }}" class="slider-img" alt="bnbfoto5">
-        <img src="{{ asset('images/bnbfoto6.png') }}" class="slider-img" alt="bnbfoto6">
-        <img src="{{ asset('images/bnbfoto7.png') }}" class="slider-img" alt="bnbfoto7">
-        <img src="{{ asset('images/bnbfoto8.png') }}" class="slider-img" alt="bnbfoto8">
-        <img src="{{ asset('images/bnbfoto9.png') }}" class="slider-img" alt="bnbfoto9">
-        <img src="{{ asset('images/bnbfoto10.png') }}" class="slider-img" alt="bnbfoto10">
-        <img src="{{ asset('images/bnbfoto11.png') }}" class="slider-img" alt="bnbfoto11">
-        <img src="{{ asset('images/bnbfoto12.png') }}" class="slider-img" alt="bnbfoto12">
-        <img src="{{ asset('images/bnbfoto13.png') }}" class="slider-img" alt="bnbfoto13">
-        <img src="{{ asset('images/bnbfoto14.png') }}" class="slider-img" alt="bnbfoto14">
-        <img src="{{ asset('images/bnbfoto15.png') }}" class="slider-img" alt="bnbfoto15">
-        <img src="{{ asset('images/bnbfoto16.png') }}" class="slider-img" alt="bnbfoto16">
+    <div class="slider-container">
+        <button class="arrow arrow-left" aria-label="Vorige foto">&lt;</button>
+
+        <div class="slider">
+            <img src="{{ asset('images/bnbfoto1.png') }}" class="slider-img active" alt="bnbfoto1">
+            <img src="{{ asset('images/bnbfoto2.png') }}" class="slider-img" alt="bnbfoto2">
+            <img src="{{ asset('images/bnbfoto3.png') }}" class="slider-img" alt="bnbfoto3">
+            <img src="{{ asset('images/bnbfoto4.png') }}" class="slider-img" alt="bnbfoto4">
+            <img src="{{ asset('images/bnbfoto5.png') }}" class="slider-img" alt="bnbfoto5">
+            <img src="{{ asset('images/bnbfoto6.png') }}" class="slider-img" alt="bnbfoto6">
+            <img src="{{ asset('images/bnbfoto7.png') }}" class="slider-img" alt="bnbfoto7">
+            <img src="{{ asset('images/bnbfoto8.png') }}" class="slider-img" alt="bnbfoto8">
+            <img src="{{ asset('images/bnbfoto9.png') }}" class="slider-img" alt="bnbfoto9">
+            <img src="{{ asset('images/bnbfoto10.png') }}" class="slider-img" alt="bnbfoto10">
+            <img src="{{ asset('images/bnbfoto11.png') }}" class="slider-img" alt="bnbfoto11">
+            <img src="{{ asset('images/bnbfoto12.png') }}" class="slider-img" alt="bnbfoto12">
+            <img src="{{ asset('images/bnbfoto13.png') }}" class="slider-img" alt="bnbfoto13">
+            <img src="{{ asset('images/bnbfoto14.png') }}" class="slider-img" alt="bnbfoto14">
+            <img src="{{ asset('images/bnbfoto15.png') }}" class="slider-img" alt="bnbfoto15">
+            <img src="{{ asset('images/bnbfoto16.png') }}" class="slider-img" alt="bnbfoto16">
+        </div>
+
+        <button class="arrow arrow-right" aria-label="Volgende foto">&gt;</button>
     </div>
 
-    <button class="arrow arrow-right">&gt;</button>
-</div>
-<!-- Modal container, standaard verborgen -->
-<div id="modal" class="modal">
-  <div class="modal-content">
-    <button id="close-modal" class="close-btn">&times;</button>
-    <img id="modal-img" src="" alt="Grote foto">
-  </div>
-</div>
-
-
-
-
-        <div class="voorzieningen-container">
-            <ul class="voorzieningen-lijst">
-                <li>
-                    <i class="fa-solid fa-euro-sign"></i> 120,- Per nacht
-                </li>
-                <li>
-                    <i class="fa-solid fa-wifi"></i> Gratis wifi
-                </li>
-                <li>
-                    <i class="fa-solid fa-eye"></i> Uitzicht
-                </li>
-                <li>
-                    <i class="fa-solid fa-house"></i> Je hebt de accommodatie voor jezelf
-                </li>
-                <li>
-                    <i class="fa-solid fa-ruler-combined"></i> 50m² grootte
-                </li>
-                <li>
-                    <i class="fa-solid fa-square-parking"></i> Gratis parkeren
-                </li>
-                <li>
-                    <i class="fa-solid fa-toilet-paper"></i> Badkamer
-                </li>
-                <li>
-                    <i class="fa-solid fa-ban-smoking"></i> Rookvrij
-                </li>
-                <li>
-                    <i class="fa-solid fa-kitchen-set"></i> Keuken
-                </li>
-                <li>
-                    <i class="fa-solid fa-shower"></i> Douche
-                </li>
-                <li>
-                    <i class="fa-solid fa-umbrella-beach"></i> Terras
-                </li>
-            </ul>
+    <!-- Modal container -->
+    <div id="modal" class="modal" role="dialog" aria-modal="true" aria-labelledby="modal-img">
+        <div class="modal-content">
+            <button id="close-modal" class="close-btn" aria-label="Sluit vergrote afbeelding">&times;</button>
+            <img id="modal-img" src="" alt="Grote foto">
         </div>
-        <script>
-    document.addEventListener('DOMContentLoaded', function () {
-        const images = document.querySelectorAll('.slider-img');
-        const left = document.querySelector('.arrow-left');
-        const right = document.querySelector('.arrow-right');
-        let index = 0;
+    </div>
 
-        function showImage(i) {
-            images.forEach(img => img.classList.remove('active'));
-            images[i].classList.add('active');
-        }
+    <div class="voorzieningen-container">
+        <ul class="voorzieningen-lijst">
+            <li><i class="fa-solid fa-euro-sign"></i> 120,- Per nacht</li>
+            <li><i class="fa-solid fa-wifi"></i> Gratis wifi</li>
+            <li><i class="fa-solid fa-eye"></i> Uitzicht</li>
+            <li><i class="fa-solid fa-house"></i> Je hebt de accommodatie voor jezelf</li>
+            <li><i class="fa-solid fa-ruler-combined"></i> 50m² grootte</li>
+            <li><i class="fa-solid fa-square-parking"></i> Gratis parkeren</li>
+            <li><i class="fa-solid fa-toilet-paper"></i> Badkamer</li>
+            <li><i class="fa-solid fa-ban-smoking"></i> Rookvrij</li>
+            <li><i class="fa-solid fa-kitchen-set"></i> Keuken</li>
+            <li><i class="fa-solid fa-shower"></i> Douche</li>
+            <li><i class="fa-solid fa-umbrella-beach"></i> Terras</li>
+        </ul>
+    </div>
 
-        left.addEventListener('click', () => {
-            index = (index - 1 + images.length) % images.length;
-            showImage(index);
-        });
+    <script>
+        document.addEventListener('DOMContentLoaded', function() {
+            const images = document.querySelectorAll('.slider-img');
+            const left = document.querySelector('.arrow-left');
+            const right = document.querySelector('.arrow-right');
+            let index = 0;
 
-        right.addEventListener('click', () => {
-            index = (index + 1) % images.length;
-            showImage(index);
-        });
+            const modal = document.getElementById('modal');
+            const modalImg = document.getElementById('modal-img');
+            const closeBtn = document.getElementById('close-modal');
 
-        // optioneel: klik om te vergroten
-        images.forEach(img => {
-            img.addEventListener('click', () => {
-                const popup = window.open("", "_blank");
-                popup.document.write(`<img src="${img.src}" style="width: 100%">`);
+            function showImage(i) {
+                images.forEach(img => img.classList.remove('active'));
+                images[i].classList.add('active');
+            }
+
+            left.addEventListener('click', () => {
+                index = (index - 1 + images.length) % images.length;
+                showImage(index);
             });
-        });
-    });
-    document.addEventListener('DOMContentLoaded', function () {
-    const images = document.querySelectorAll('.slider-img');
-    const left = document.querySelector('.arrow-left');
-    const right = document.querySelector('.arrow-right');
-    let index = 0;
 
-    const modal = document.getElementById('modal');
-    const modalImg = document.getElementById('modal-img');
-    const closeBtn = document.getElementById('close-modal');
+            right.addEventListener('click', () => {
+                index = (index + 1) % images.length;
+                showImage(index);
+            });
 
-    function showImage(i) {
-        images.forEach(img => img.classList.remove('active'));
-        images[i].classList.add('active');
-    }
-
-    left.addEventListener('click', () => {
-        index = (index - 1 + images.length) % images.length;
-        showImage(index);
-    });
-
-    right.addEventListener('click', () => {
-        index = (index + 1) % images.length;
-        showImage(index);
-    });
-
-    // Modal openen bij klikken op foto
-    images.forEach(img => {
-        img.addEventListener('click', () => {
-            modal.style.display = 'flex';
-            modalImg.src = img.src;
-        });
-    });
-
-    // Modal sluiten met kruisje
-    closeBtn.addEventListener('click', () => {
-        modal.style.display = 'none';
-        modalImg.src = '';
-    });
-
-    // Optioneel: klik buiten de afbeelding sluit ook modal
-    modal.addEventListener('click', (e) => {
-        if (e.target === modal) {
-            modal.style.display = 'none';
-            modalImg.src = '';
-        }
+            // Modal openen bij klikken op foto
+            images.forEach(img => {
+    img.addEventListener('click', () => {
+        console.log('Clicked image src:', img.src);
+        modal.style.display = 'flex';
+        modalImg.src = img.src;
     });
 });
 
-</script>
+
+            // Modal sluiten met kruisje
+            closeBtn.addEventListener('click', () => {
+                modal.style.display = 'none';
+                modalImg.src = '';
+            });
+
+            // Klik buiten modal-content sluit ook modal
+            modal.addEventListener('click', e => {
+                if (e.target === modal) {
+                    modal.style.display = 'none';
+                    modalImg.src = '';
+                }
+            });
+        });
+    </script>
 
 </body>
 
