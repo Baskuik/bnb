3ba7a0330d37c2cdc94521a2cf84ae2feea53c01
Baskuik <97105236@st.deltion.nl>
2025-06-26 19:44:36 +0200

    jbjk

--- Bnb/routes/web.php
+++ Bnb/routes/web.php
@@ -64,14 +64,5 @@ Route::post('password/email', [ForgotPasswordController::class, 'sendResetLinkEm
 Route::get('password/reset/{token}', [ResetPasswordController::class, 'showResetForm'])->name('password.reset');
 Route::post('password/reset', [ResetPasswordController::class, 'reset'])->name('password.update');
 
-
-
 Route::get('/boeking/confirm', [BoekingController::class, 'confirm'])->name('boeking.confirm');
-
-
-
-
-
-
-
 
